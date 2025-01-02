83a88be05b63b4a2b5b7ae05aa00ca30783c1ad1
Moein Heshmati <moeinheshmati1383@gmail.com>
2025-01-02 18:49:42 +0330

    Upload Art Python project on Github

---
+++ art.py
@@ -0,0 +1,23 @@
+# Library
+from turtle import *
+from colorsys import *
+
+# Title
+title("Art With Python")
+bgcolor('black')
+pensize(3)
+speed(0)
+h = 0
+
+# Draw
+for i in range(140):
+    color = hsv_to_rgb(h, 1, 1)
+    pencolor(color)
+    h += 0.01
+    circle(i, 100)
+    right(80)
+    circle(i, -50)
+done()
+
+# Create By Moein Heshmati
+# Finish
